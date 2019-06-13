5de330a64c86246e1fcc8febe224719c1d4aa7de
rxlacroix <romain.lacroix@free.fr>
2019-06-13 21:35:27 +0200

    clean

--- R/makeTilegram.R
+++ R/makeTilegram.R
@@ -110,66 +110,65 @@ hex_tiles <- function(x, cellsize=NULL){
 #' then uses the 'Hungarian' algorithm found in the `clue` package to match hexagons to
 #' Polygons by minimising the distance between the centre of the hexagon and the centroid of the polygon.
 #'
-#' @param sp A SpatialPolygonDataFrame
+#' @param sp A sf object
 #'
-#' @return A SpatialPolygonsDataFrame projected to EPSG:32663 equidistant grid
+#' @return A sf object projected to EPSG:32663 equidistant grid
 #' @export
 #'
 #' @examples
-#' require(rworldmap);
-#' data("countriesCoarseLessIslands") #  Load simple map without islands
-#' afr <- countriesCoarseLessIslands[which(!is.na(countriesCoarseLessIslands@data$REGION) &
-#'                                          countriesCoarseLessIslands@data$REGION=="Africa"),]
-#' tileGram <- makeTilegram(afr)
-#' plot(tileGram)
+#' nc <- st_read(system.file("shape/nc.shp", package="sf"))
+#' ncTileGram <- makeTilegram(nc)
+#' plot(ncTileGram["id"])
+
+
 makeTilegram <- function(sp){
 
-sp$id <- row.names(sp)
+  sp$id <- row.names(sp)
 
-sp <- st_cast(sp, to="POLYGON")
+  sp <- st_cast(sp, to="POLYGON")
 
-sp$carea <- as.numeric(st_area(sp))
-sp <- sp[order(sp$carea, decreasing = TRUE),]
+  sp$carea <- as.numeric(st_area(sp))
+  sp <- sp[order(sp$carea, decreasing = TRUE),]
 
 
-sp <- sp[!duplicated(sp$id),]
+  sp <- sp[!duplicated(sp$id),]
 
-sp <- st_transform(sp, 32663)
-tiles <- hex_tiles(as(sp, 'Spatial'))
-sp <- st_make_valid(st_as_sf(sp))
-tiles <- st_make_valid(st_as_sf(tiles))
-tiles <- tiles[sp, ]
-pts <- st_centroid(sp)
-rm(sp)
-gc()
-pts$pt_id <- row.names(pts)
-tileCentroids <- st_centroid(tiles)
-tileCentroids$id <- row.names(tileCentroids)
-tile_pref <- data.frame()
-for (i in 1:nrow(pts)) {
+  sp <- st_transform(sp, 32663)
+  tiles <- hex_tiles(as(sp, 'Spatial'))
+  sp <- st_make_valid(st_as_sf(sp))
+  tiles <- st_make_valid(st_as_sf(tiles))
+  tiles <- tiles[sp, ]
+  pts <- st_centroid(sp)
+  rm(sp)
   gc()
-  distance <- drop_units(st_distance(pts[i,], tileCentroids, by_element =F))
-  colnames(distance) <- tileCentroids$id
-  y <- t(apply(distance,1, function(x) rank(x,ties.method ="random")))
-  tile_pref <- rbind(tile_pref, y)
-  cat("Feature ", i, "on ", nrow(pts), " : ", (i/nrow(pts)*100), " % \n")
-}
-tile_pref <- as.matrix(tile_pref)
- solved <- clue::solve_LSAP(tile_pref, maximum = FALSE)
- solved_cols <- c(as.numeric(solved))
- tile_pref <- as.data.frame(tile_pref)
- id_solved <- vector()
- for (i in 1:nrow(tile_pref)) {
-   id_solved[i] <- as.numeric(colnames(tile_pref[solved_cols[i]]))
- }
+  pts$pt_id <- row.names(pts)
+  tileCentroids <- st_centroid(tiles)
+  tileCentroids$id <- row.names(tileCentroids)
+  tile_pref <- data.frame()
+  for (i in 1:nrow(pts)) {
+    gc()
+    distance <- drop_units(st_distance(pts[i,], tileCentroids, by_element =F))
+    colnames(distance) <- tileCentroids$id
+    y <- t(apply(distance,1, function(x) rank(x,ties.method ="random")))
+    tile_pref <- rbind(tile_pref, y)
+    cat("Feature ", i, "on ", nrow(pts), " : ", (i/nrow(pts)*100), " % \n")
+  }
+  tile_pref <- as.matrix(tile_pref)
+  solved <- clue::solve_LSAP(tile_pref, maximum = FALSE)
+  solved_cols <- c(as.numeric(solved))
+  tile_pref <- as.data.frame(tile_pref)
+  id_solved <- vector()
+  for (i in 1:nrow(tile_pref)) {
+    id_solved[i] <- as.numeric(colnames(tile_pref[solved_cols[i]]))
+  }
 
- newDat <- data.frame(tile_region= row.names(tile_pref), id = id_solved, stringsAsFactors = F)
- newTiles <- tiles
+  newDat <- data.frame(tile_region= row.names(tile_pref), id = id_solved, stringsAsFactors = F)
+  newTiles <- tiles
 
 
 
-newTiles <- full_join(newTiles, newDat, by="id")
-newTiles <- newTiles[!is.na(newTiles$tile_region),]
+  newTiles <- full_join(newTiles, newDat, by="id")
+  newTiles <- newTiles[!is.na(newTiles$tile_region),]
 
-return(newTiles)
+  return(newTiles)
 }
--- README.md
+++ README.md
@@ -8,74 +8,10 @@ For large sf datasets
 
 A tilegram is a tiled map where regions are adjusted to have the same size.
 
-```{r,message=F, warning=F, results='asis',fig.width=5, fig.height=4.5}
-# Install package
-if(!"makeTilegram" %in% installed.packages()[,"Package"])
-  devtools::install_git("https://gitlab.com/lajh87/makeTilegram")
-
-# Load required packages
-require(makeTilegram)
-require(rworldmap)
-
-#  Load simple map (without islands) from the `rworldmap` package
-data("countriesCoarseLessIslands") 
-
-# Subset for Africa and remove NAs in the regions
-afr <- countriesCoarseLessIslands[which(!is.na(countriesCoarseLessIslands@data$REGION) &  
-                                          countriesCoarseLessIslands@data$REGION=="Africa"),]
-
-
-tileGram <- makeTilegram(afr) # Make a Tilegram
-plot(tileGram)
 ```
-
-
-## Comparison of tileGram to Actual Location
-
-```{r, message=F, warning=F,cache=T}
-require(ggmap)
-require(ggplot2)
-
-tileGram <- spTransform(tileGram, "+init=epsg:4326")
-
-# Convert to a data.frame
-tileGram_f <- ggplot2::fortify(tileGram) 
-tileGram_f <- plyr::join(tileGram_f, tileGram@data[,c("id", "tile_region")],by="id") # Join with original data
-
-m <- ggmap(get_map(location = "Congo", zoom=3)) 
-m + geom_polygon(data=tileGram_f, aes(long, lat, group = group), fill="steelblue", colour="blue", alpha=0.5) +coord_equal() +
-   theme(panel.background = element_blank(), 
-        axis.title = element_blank(), axis.text = element_blank(), axis.ticks = element_blank())
+  devtools::install_github("rxlacroix/tilegram")
+ nc <- st_read(system.file("shape/nc.shp", package="sf"))
+ ncTileGram <- makeTilegram(nc)
+ plot(ncTileGram["id"])
 
 ```
-
-## Interactive Plot using Plotly
-
-The code chunk below shows how to build the plot using `ggplot2` and add interactivity using `plotly`.
-
-```{r, warning=F, message=F,fig.width=5, fig.height=4.5}
-require(plotly)
-
-## Join to population and GDP data
-df <- afr@data[,c("GEOUNIT", "POP_EST", "GDP_MD_EST")]
-names(df)[1] <- "tile_region"
-df$`GDP Per Capita`  <- round((df$GDP_MD_EST*1e6)/(df$POP_EST),-1)
-tileGram_f <- plyr::join(tileGram_f, df)
-
-names(tileGram_f)[names(tileGram_f)=="tile_region"] <- "Country"
-names(tileGram_f)[names(tileGram_f)=="GDP Per Capita"] <- "GDP_Capita"
-tileGram_f$value <- paste0("$",format(tileGram_f$GDP_Capita,big.mark = ","))
-
-## Build plot in `ggplot2`
-p <- ggplot(tileGram_f, aes(label = Country, text=paste("GDP per Capita:", value))) +
-  geom_polygon(aes(long, lat, group = group, fill=log(`GDP_Capita`))) +coord_equal() +
-  theme(panel.background = element_blank(), 
-        axis.title = element_blank(), axis.text = element_blank(), axis.ticks = element_blank()) +
-  scale_fill_continuous(name = "GDP per head (USD)",labels=function(x) paste0("$",format(round(exp(x),-3),big.mark = ",",scientific = F)))
-
-## Add interactivity with plotly
-ggplotly(p,tooltip = c("Country", "text")) %>% config(displayModeBar=F)
-
-```
-
-
